cd5d6d4c0f0f52e31b574c585bc69612af564d83
Kimberly Ann Meadows <kimmeadows8@gmail.com>
2019-11-12 01:53:17 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ const gifts = ["teddy bear", "drone", "doll"];
 //   for (let i = 0; i < gifts.length; i++) {
 //     console.log(`Wrapped ${gifts[i]} and added a bow!`);
 //   }
-// 
+//
 //   return gifts;
 // }
 
