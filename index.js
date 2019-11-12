82280ea2fc09ea16a351d50ccff3b80ce4002163
Kimberly Ann Meadows <kimmeadows8@gmail.com>
2019-11-12 01:35:44 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solutions in this file
+const names = ["Ada", "Brendan", "Ali"]
+function writeCards(name) {
+  for (let i = 0; i < names.length; i++) {
+  console.log('Thank you, ${name}, for the wonderful birthday gift.');
+}
+return names;
+}
+
+function countdown(){
+  let countdown = 10;
+  while (countdown > 0) {
+    console.log(countdown++);
+  }
+}
